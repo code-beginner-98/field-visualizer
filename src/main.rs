96aba2b657a5323135a684e449188b16061838c9
code-beginner-98 <92854522+code-beginner-98@users.noreply.github.com>
2026-01-09 14:43:34 +0100

    added arrow painting, removed vector and coordinate, because egui already has Pos2 and Vec2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::default;
+
+use egui::{emath::RectTransform, Color32, Pos2, Rect, Sense, Stroke, Vec2};
+
 fn main() -> eframe::Result<()>
 {
     // runs the app
@@ -26,6 +30,22 @@ impl eframe::App for VisApp
         egui::CentralPanel::default().show(&ctx, |ui|
         {
             ui.label("Hello, world!");
+            let (response, painter) = ui.allocate_painter(
+                ui.available_size_before_wrap(),
+                Sense::drag()
+            );
+            let painter_proportions = response.rect.square_proportions();
+            let to_screen = RectTransform::from_to(
+                Rect::from_min_size(
+                    Pos2::ZERO - painter_proportions,
+                    2. * painter_proportions),
+                response.rect,);
+            let arrow = Arrow::default();
+            painter.arrow(
+                to_screen * arrow.origin,
+                to_screen.scale() * arrow.direction,
+                Stroke::new(1.0, Color32::RED)
+            );
         });
     }
 }
@@ -40,19 +60,18 @@ impl VisApp
 
 struct Arrow
 {
-    a: Coordinate,
-    b: Coordinate,
-    dir: Vector,
+    origin: Pos2,
+    direction: Vec2,
 }
 
-struct Coordinate
+impl Default for Arrow
 {
-    x: i32,
-    y: i32,
-}
-
-struct Vector
-{
-    x: i32,
-    y: i32,
+    fn default() -> Self
+    {
+        Self
+        {
+            origin: Pos2::new(0., 0.),
+            direction: Vec2::new(1., 1.)
+        }
+    }
 }
