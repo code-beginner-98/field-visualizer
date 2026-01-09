4202bab5f326f971d2bceb4107cec9daafd4a016
code-beginner-98 <92854522+code-beginner-98@users.noreply.github.com>
2026-01-09 13:50:53 +0100

    basic hello world egui app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> eframe::Result<()>
+{
+    // runs the app
+    eframe::run_native(
+        "Field Visualizer",
+        eframe::NativeOptions::default(),
+        Box::new(|_cc| Ok(Box::new(VisApp::new()))),
+    )
+}
+
+struct VisApp
+{}
+
+impl Default for VisApp
+{
+    fn default() -> Self
+    {
+        Self {}
+    }
+}
+
+impl eframe::App for VisApp
+{
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame)
+    {
+        egui::CentralPanel::default().show(&ctx, |ui|
+        {
+            ui.label("Hello, world!")
+        });
+    }
+}
+
+impl VisApp
+{
+    fn new() -> Self
+    {
+        Default::default()
+    }
+}
+
+struct Arrow
+{
+    a: Coordinate,
+    b: Coordinate,
+    dir: Vector,
+}
+
+struct Coordinate
+{
+    x: i32,
+    y: i32,
+}
+
+struct Vector
+{
+    x: i32,
+    y: i32,
 }
